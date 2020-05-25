38dd1859f1b82e068f3eb092f85cf225aa4dc208
Timur Berezov <berezov.timur@gmail.com>
2020-05-25 11:51:24 +0300

    Change in Smartphone

--- src/main/java/ru/netology/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/ru/netology/manager/ProductManager.java
@@ -2,7 +2,7 @@ package ru.netology.ru.netology.manager;
 
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
-import ru.netology.domain.ru.netology.domain.Smartphone;
+import ru.netology.domain.Smartphone;
 import ru.netology.repository.ProductRepository;
 
 public class ProductManager {
